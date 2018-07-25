90d48ed51bc721db497760b760d24a8081fdd91c
Tessa Gonzales <tessaleego@gmail.com>
2018-07-25 15:37:57 -0700

    fingers crossed......

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   production: {
       client: 'pg',
-      connection: process.env.DATABASE_URL,
+      connection: process.env.HEROKU_POSTGRESQL_URL,
       migrations: {
           directory: __dirname + '/db/migrations',
         },
