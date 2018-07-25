e66014d0745ab5146ee71c11d0ce7d7c0233c9a5
Tessa Gonzales <tessaleego@gmail.com>
2018-07-25 15:45:27 -0700

    blah`:

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   production: {
       client: 'pg',
-      connection: process.env.HEROKU_POSTGRESQL_URL,
+      connection: process.env.DATABASE_URL,
       migrations: {
           directory: __dirname + '/db/migrations',
         },
